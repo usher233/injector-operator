5e3ec9b26407d5e9be3c4330053a3966799b48e1
Yuhang Wu <Yuhang.Wu@swisscom.com>
2024-12-24 13:58:10 +0100

    logic and crd

--- internal/controller/injector_controller.go
+++ internal/controller/injector_controller.go
@@ -20,11 +20,13 @@ import (
 	"context"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	injectorv1alpha1 "github.com/usher233/injector-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 // InjectorReconciler reconciles a Injector object
@@ -47,9 +49,43 @@ type InjectorReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.1/pkg/reconcile
 func (r *InjectorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	l := log.FromContext(ctx)
 
 	// TODO(user): your logic here
+	injector := &injectorv1alpha1.Injector{}
+	if err := r.Client.Get(ctx, req.NamespacedName, injector); err != nil {
+		l.Info("unable to fetch Injector", "error", err)
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	podList := &corev1.PodList{}
+	if err := r.Client.List(ctx, podList, client.InNamespace(req.Namespace)); err != nil {
+		l.Info("unable to list Pods", "error", err)
+		return ctrl.Result{}, err
+	}
+
+	if len(podList.Items) == 0 {
+		l.Info("no Pods found")
+		pod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "nginx-pod",
+				Namespace: req.Namespace,
+			},
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{
+						Name:  "injector",
+						Image: "nginx:latest",
+					},
+				},
+			},
+		}
+		if err := r.Client.Create(ctx, pod); err != nil {
+			l.Info("unable to create Pod", "error", err)
+			return ctrl.Result{}, err
+		}
+	}
+
 
 	return ctrl.Result{}, nil
 }
@@ -57,7 +93,8 @@ func (r *InjectorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 // SetupWithManager sets up the controller with the Manager.
 func (r *InjectorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&injectorv1alpha1.Injector{}).
-		Named("injector").
-		Complete(r)
+    For(&injectorv1alpha1.Injector{}).
+    Owns(&corev1.Pod{}).
+    Named("injector").
+    Complete(r)
 }
